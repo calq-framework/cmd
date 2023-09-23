d505e3a96ace5e20eff159d6a7f5c5234ac2ba88
greg-chuchro <gchuchro@acm.org>
2023-09-23 21:57:55 +0900

    shell util

---
+++ Ghbvft6.Calq.Terminal/CommandExecutionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghbvft6.Calq.Terminal;
+public class CommandExecutionException : Exception
+{
+    public int ExitCode { get; }
+
+    public CommandExecutionException(string message, int exitCode) : base(message)
+    {
+        ExitCode = exitCode;
+    }
+}
