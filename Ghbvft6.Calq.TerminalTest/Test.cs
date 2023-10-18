4ea7c17cc1bae4c35b4a015233c43a3af26b1e8e
greg-chuchro <gchuchro@acm.org>
2023-10-18 20:04:27 +0900

    fails on redirected input (#5)

--- Ghbvft6.Calq.TerminalTest/Test.cs
+++ Ghbvft6.Calq.TerminalTest/Test.cs
@@ -16,4 +16,15 @@ public class Test
 
         Assert.NotEqual("", output.Trim());
     }
+
+    // TODO detect Console.SetIn as input redirect or figure out a test with input redirect from file
+    //[Fact]
+    //public void RedirectedInput() {
+    //    var input = "hello world\n";
+    //    Console.SetIn(new StringReader(input));
+
+    //    var output = BashUtil.CMD("sleep 1; read -r input; echo $input");
+
+    //    Assert.NotEqual(input, output);
+    //}
 }
