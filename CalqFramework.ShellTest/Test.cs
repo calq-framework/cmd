19c0928c55147842a5949e4cf2bb47c6d212cba1
greg-chuchro <gchuchro@acm.org>
2025-02-06 17:53:42 +0900

    fix RelayStream

--- CalqFramework.ShellTest/Test.cs
+++ CalqFramework.ShellTest/Test.cs
@@ -22,4 +22,24 @@ public class Test
 
         Assert.Equal(input, output);
     }
+
+    [Fact]
+    public void LongOutputTest()
+    {
+        var expectedOutput = "";
+        for (var i = 0; i < 10000; ++i) {
+            expectedOutput += "T";
+        }
+
+        var writer = new StringWriter();
+        Console.SetIn(new StringReader(expectedOutput));
+        Console.SetOut(writer);
+
+        ShellUtil.SetShell(new Bash());
+        var output = new Bash().CMD("sleep 1; printf 'T'%.0s {1..5000}; sleep 1; printf 'T'%.0s {1..5000}");
+        var writerOutput = writer.ToString();
+
+        Assert.Equal(expectedOutput, output);
+        Assert.Equal(expectedOutput, writerOutput);
+    }
 }
