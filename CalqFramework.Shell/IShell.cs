a4f091e4169a587472362c1d5acf5e5caa6d62e7
greg-chuchro <gchuchro@acm.org>
2025-03-07 20:39:01 +0900

    timeout

--- CalqFramework.Shell/IShell.cs
+++ CalqFramework.Shell/IShell.cs
@@ -1,11 +1,15 @@
-﻿namespace CalqFramework.Shell;
+﻿
+namespace CalqFramework.Shell {
+    public interface IShell {
+        string CurrentDirectory { get; }
+        TextReader In { get; init; }
+        TextWriter Out { get; init; }
 
-// TODO extend TextWriter (requires statefull shells)
-// stateful bash could be done maybe with subshells ( script ) > file ?
-public interface IShell {
-    string CurrentDirectory { get; }
-    string CMD(string script, TextReader? inputReader = null);
-    void RUN(string script, TextReader? inputReader = null);
-    void CD(string path);
-    string GetLocalPath(string path);
+        void CD(string path);
+        string CMD(string script, TimeSpan? cancellationDelay = null);
+        string CMD(string script, TextReader inputReader, TimeSpan? cancellationDelay = null);
+        string GetLocalPath(string path);
+        void RUN(string script, TimeSpan? cancellationDelay = null);
+        void RUN(string script, TextReader inputReader, TimeSpan? cancellationDelay = null);
+    }
 }
