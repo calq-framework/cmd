654fbe14f754784e939b3ee9fcda0d53bfcef097
greg-chuchro <gchuchro@acm.org>
2025-05-08 13:54:19 +0900

    Python shell

---
+++ CalqFramework.Cmd/Shells/Python.cs
@@ -0,0 +1,25 @@
+﻿using CalqFramework.Cmd.Shell;
+
+namespace CalqFramework.Cmd.Shells;
+public class Python : ShellBase {
+
+    public Python(IPythonServer pythonServer) {
+        var handler = new HttpClientHandler {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+        var httpClient = new HttpClient(handler);
+        httpClient.BaseAddress = pythonServer.Uri;
+
+        HttpClient = httpClient;
+    }
+
+    public HttpClient HttpClient { get; }
+
+    public override IShellWorker CreateShellWorker(ShellScript shellScript, Stream? inputStream) {
+        return new HttpShellWorker(HttpClient, shellScript, inputStream);
+    }
+
+    public override string MapToInternalPath(string hostPath) {
+        return hostPath;
+    }
+}
