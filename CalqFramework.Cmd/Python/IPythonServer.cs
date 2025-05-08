22440db9d37347dc385f237f4da586afb433e847
greg-chuchro <gchuchro@acm.org>
2025-05-08 13:54:19 +0900

    Python shell

---
+++ CalqFramework.Cmd/Python/IPythonServer.cs
@@ -0,0 +1,9 @@
+﻿using CalqFramework.Cmd.Shell;
+
+namespace CalqFramework.Cmd.Shells {
+    public interface IPythonServer {
+        Uri Uri { get; }
+
+        Task<IShellWorker> StartAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ CalqFramework.CmdTest/PythonTest.cs
@@ -0,0 +1,37 @@
+﻿using CalqFramework.Cmd.Shells;
+using System.Text;
+using static CalqFramework.Cmd.Terminal;
+
+namespace CalqFramework.CmdTest;
+
+public class PythonTestTest {
+    private Stream GetStream(string input) {
+        byte[] byteArray = Encoding.ASCII.GetBytes(input);
+        MemoryStream stream = new MemoryStream(byteArray);
+        return stream;
+    }
+
+    private string ReadString(Stream writer) {
+        writer.Position = 0;
+        using StreamReader reader = new StreamReader(writer);
+        return reader.ReadToEnd();
+    }
+
+    [Fact]
+    public async Task Python_ReadInput_EchosCorrectly() {
+        var input = "hello world\nhello world\n";
+        var writer = new MemoryStream();
+        LocalTerminal.Out = writer;
+
+        var pythonServer = new PythonServer("./python_cli_tool.py");
+        await pythonServer.StartAsync();
+        LocalTerminal.Shell = new Python(pythonServer) {
+            In = GetStream(input)
+        };
+
+        RUN("test");
+
+        var output = ReadString(writer);
+        Assert.Equal(input, output);
+    }
+}
