4f6182131164337698c82fb2cedb3a7a4a6eabd8
greg-chuchro <gchuchro@acm.org>
2025-11-03 22:25:57 +0900

    update test_tool.py

--- CalqFramework.CmdTest/test_tool.py
+++ CalqFramework.CmdTest/test_tool.py
@@ -1,19 +1,17 @@
-﻿import fire
-import asyncio
+﻿import asyncio
 import sys
 import io
 
-async def delayed_echo_gen():
+async def test():
     for line in sys.stdin:
         await asyncio.sleep(1)
         yield line
 
-def test():
-    return delayed_echo_gen()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import fire
+    fire.core._PrintResult = lambda component_trace, verbose=False, serialize=None: None
     value = fire.Fire()
     async def main():
-        async for line in value:
-            print(line)
+        async for part in value:
+            print(part, end="")
     asyncio.run(main())
