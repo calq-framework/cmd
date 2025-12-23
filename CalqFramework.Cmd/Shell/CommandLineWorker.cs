3fa21aefcf125ce0beaf8461ec8e92dfd341ad77
greg-chuchro <gchuchro@acm.org>
2025-12-23 15:05:48 -0800

    enable single-word commands for CommandLineWorker

--- CalqFramework.Cmd/Shell/CommandLineWorker.cs
+++ CalqFramework.Cmd/Shell/CommandLineWorker.cs
@@ -3,6 +3,11 @@
     public class CommandLineWorker(ShellScript shellScript, Stream? inputStream, bool disposeOnCompletion = true) : ProcessWorkerBase(shellScript, inputStream, disposeOnCompletion) {
         internal override ProcessExecutionInfo GetProcessExecutionInfo(string workingDirectory, string script) {
             int spaceIndex = script.IndexOf(' ');
+            if (spaceIndex == -1) {
+                // Single-word command with no arguments
+                return new ProcessExecutionInfo(script, "");
+            }
+            
             string command = script.Substring(0, spaceIndex);
             string arguments = script.Substring(spaceIndex + 1);
             return new ProcessExecutionInfo(command, arguments);
