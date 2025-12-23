9e1688138d3854ac7883f985a01ef9a4dfbbc183
greg-chuchro <gchuchro@acm.org>
2025-12-22 17:46:34 -0800

    test_throw_exception for python server

--- CalqFramework.Cmd/Python/test_tool.py
+++ CalqFramework.Cmd/Python/test_tool.py
@@ -7,11 +7,19 @@ async def test():
         await asyncio.sleep(1)
         yield line
 
+async def test_throw_exception():
+    for line in sys.stdin:
+        await asyncio.sleep(1)
+        yield line
+        raise Exception("An intentional error occurred.")
+
 if __name__ == "__main__":
     import fire
     fire.core._PrintResult = lambda component_trace, verbose=False, serialize=None: None
     value = fire.Fire()
+    
     async def main():
         async for part in value:
             print(part, end="")
+            
     asyncio.run(main())
