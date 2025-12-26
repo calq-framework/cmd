bf320d3077f76179af54dde0db758dc4bceb333d
greg-chuchro <gchuchro@acm.org>
2025-12-25 22:24:43 -0800

    public TerminalLogger

--- CalqFramework.Cmd/TerminalComponents/TerminalLogger.cs
+++ CalqFramework.Cmd/TerminalComponents/TerminalLogger.cs
@@ -1,10 +1,9 @@
 ﻿namespace CalqFramework.Cmd.TerminalComponents {
 
-    internal class TerminalLogger : ITerminalLogger {
+    public class TerminalLogger : ITerminalLogger {
         public TextWriter Out { get; init; } = Console.Out;
 
-        public void LogRun(ShellScript shellScript) {
-            Out.WriteLine($"\nDIR: {shellScript.WorkingDirectory}");
+        public void Log(ShellScript shellScript) {
             if (!shellScript.Script.Contains('\n')) {
                 Out.WriteLine($"RUN: {shellScript.Script}");
             } else {
