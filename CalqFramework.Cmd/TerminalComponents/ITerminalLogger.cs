583d2f3c1fb32119b47d8e7a708fdf8bbc6ae08c
greg-chuchro <gchuchro@acm.org>
2025-12-25 22:24:43 -0800

    public TerminalLogger

--- CalqFramework.Cmd/TerminalComponents/ITerminalLogger.cs
+++ CalqFramework.Cmd/TerminalComponents/ITerminalLogger.cs
@@ -3,10 +3,10 @@
     public interface ITerminalLogger {
 
         /// <summary>
-        /// Logs the execution of a shell script to the output stream.
-        /// Displays the working directory and command being executed for debugging purposes.
+        /// Logs a shell script before it is executed in RUN operations.
+        /// Called only for interactive command execution, not for CMD operations that return string results.
         /// </summary>
-        /// <param name="shellScript">The shell script to log information about</param>
-        public void LogRun(ShellScript shellScript);
+        /// <param name="shellScript">The shell script about to be executed</param>
+        public void Log(ShellScript shellScript);
     }
 }
